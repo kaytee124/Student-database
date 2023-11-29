f898ce64ac1ae5546ae5d614047e10afcb3dcda8
kaytee124 <ktproductions124@gmail.com>
2023-11-29 01:44:59 +0000

    Databse project

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
 
+import subprocess
+
+# List of packages to install
+packages = ["streamlit", "mysql-connector-python"]
+
+# Install each package using pip
+for package in packages:
+    subprocess.check_call(["pip", "install", package])
 
 import mysql.connector
 
